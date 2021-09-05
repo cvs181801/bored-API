16d777fb973dbde15d519130483f08b63d8113f4
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-05 13:39:30 -0700

    fix the initial screen

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 
 const boredDiv = document.querySelector(".activity");
 const button = document.querySelector(".image__newactivitybtn");
+const buttonContainer = document.querySelector(".BtnContainer");
 let activity = document.createElement("p");
 let type = document.createElement("p");
 let costAssociated = document.createElement("p");
 
+buttonContainer.style.marginTop = "15em";
+
 button.addEventListener("click", function(e) {
+    
     fetch("http://www.boredapi.com/api/activity/")  //https://apis.scrimba.com/bored/api/activity these are added to scrimba platform FYI
     .then(res => res.json())
     .then(data => {
+        buttonContainer.style.marginTop = "auto";
+      
         activity.textContent = "";
         type.textContent = "";
         costAssociated.textContent = "";
