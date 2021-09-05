88680ccfc14024cdb1e12e05ed91e28a596eaf66
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-05 10:56:19 -0700

    build bored bot DOM manipulation

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ const boredDiv = document.querySelector(".activity");
 
 fetch("https://apis.scrimba.com/bored/api/activity")
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => {
+        let activity = document.createElement("p");
+        activity.textContent = `Activity: ${data.activity}`;
+        boredDiv.append(activity);
+        let type = document.createElement("p");
+        type.textContent = `Type: ${data.type}`;
+        boredDiv.append(type);
+        let costAssociated = document.createElement("p");
+        if (`${data.price}` == 0) {
+            costAssociated.textContent = `Does it cost $$$?: No`;
+        } else {
+            costAssociated.textContent = `Does it cost $$$?: Yes`;
+        }
+        boredDiv.append(costAssociated);
+    });
+
